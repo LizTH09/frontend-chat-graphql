28348140478d7197b7e94fe826a2ddec2ee9f13d
Lizbeth Aldana <sele99na@gmail.com>
2024-12-02 22:33:27 -0500

    style: changing some styles

--- src/components/ChatRoom/index.tsx
+++ src/components/ChatRoom/index.tsx
@@ -32,15 +32,33 @@ const ChatRoomComponent: FC<ChatRoomProps> = ({ chatId, userId }) => {
         flexDirection: "column",
         justifyContent: "space-between",
         height: "60vh",
-        width: "600px",
-        border: "1px solid lightgray",
-        borderRadius: "15px",
-        padding: "25px",
+        width: "550px",
+        border: "1px solid #e0e0e0",
+        borderRadius: "12px",
+        padding: "20px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        backgroundColor: "#fff",
       }}
     >
-      <Typography variant="h5" sx={{ textAlign: "center", margin: "5px" }}>
-        Chat Room
-      </Typography>
+      <Box
+        sx={{
+          marginBottom: "10px",
+          borderBottom: "1px solid #e0e0e0",
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{
+            textAlign: "center",
+            margin: "5px",
+            marginBottom: "15px",
+            fontWeight: "600",
+            color: "#1976d2",
+          }}
+        >
+          Chat Room
+        </Typography>
+      </Box>
       <MessageListComponent chatId={chatId} userId={userId} />
       <ChatInputComponent onClickSendMessage={_handleSendMessage} />
     </Box>
